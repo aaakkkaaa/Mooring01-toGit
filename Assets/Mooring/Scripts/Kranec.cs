8b5babb7d99fc7854902778a105019c50abe58b6
fsamin <fsamin@yandex.ru>
2020-04-04 05:38:45 +0300

    Кранец, начало работы

---
+++ Assets/Mooring/Scripts/Kranec.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+public class Kranec : MonoBehaviour
+{
+    public GameObject Layer;
+
+    private GameObject _ballon;
+    private ObiRope _rope;
+    private GameObject _zero;
+    private ObiRopeBlueprint _bluePrint;
+
+    public bool Set;
+
+    private void Awake()
+    {
+        _ballon = transform.Find("Ballon").gameObject;
+        _rope = transform.Find("Rope").GetComponent<ObiRope>();
+        _bluePrint = _rope.blueprint as ObiRopeBlueprint;
+        _zero = transform.Find("ZeroPoint").gameObject;
+    }
+
+    private void Update()
+    {
+        if (Set)
+        {
+            SetAndKnot();
+            Set = false;
+        }
+    }
+
+    public void SetAndKnot()
+    {
+        if (Layer == null) return;
+
+        // поднести кранец близко к точке привязки
+        transform.position = Layer.transform.position;
+        transform.rotation = Layer.transform.rotation;
+        Vector3 pos = transform.localPosition;
+        pos.x -= 0.3f;
+        transform.localPosition = pos;
+
+        StartCoroutine(MoveToLayer());
+    }
+
+    private IEnumerator MoveToLayer()
+    {
+        while ((Layer.transform.position - _zero.transform.position).magnitude > 0.01f)
+        {
+            _zero.transform.position = Vector3.Lerp(_zero.transform.position, Layer.transform.position, 0.05f);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
