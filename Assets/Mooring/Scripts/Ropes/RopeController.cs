b1f954a3a97fa8600127fef4cc9616099e604cf2
fsamin <fsamin@yandex.ru>
2020-04-23 00:50:19 +0300

    Бросок веревки

---
+++ Assets/Mooring/Scripts/Ropes/RopeController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using System;
+using Obi;
+
+// Взаимодействие с персонажами, броски, закрепление
+public class RopeController : MonoBehaviour
+{
+    public float Stretch = 1.3f;
+    public float MaxForce = 2000;
+
+    // для таскания рукой за одну или несколько точек
+    private float springStiffness = 5000;
+    private float springDamping = 50;
+    private int Interval = 4;        // на сколько еще частиц в обе стороны применить силу
+    [NonSerialized]
+    public List<int> FixPoints;         // точки, притягиваемые к руке
+    [NonSerialized]
+    public GameObject Fixator;          // объект, к которому идет притяжение
+
+    private ObiSolver _solver;
+    private ObiRope _rope;
+
+    // для выбора поведения в FixedUpdate
+    [NonSerialized]
+    public string[] States = { "ONEPOINT", "MANYPOINTS", "FREE" };
+    [NonSerialized]
+    public string CurState = "FREE";    // ONEPOINT, MANYPOINTS, FREE
+
+
+    private void Awake()
+    {
+        FixPoints = new List<int>();
+    }
+
+    private void OnEnable()
+    {
+        _rope = GetComponent<ObiRope>();
+        _solver = _rope.solver;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (CurState == "" || CurState == "FREE") return;
+        if (_solver == null)
+        {
+            _rope = GetComponent<ObiRope>();
+            _solver = _rope.solver;
+        }
+
+
+        if (CurState == "ONEPOINT")
+        {
+            if(Fixator != null)
+            {
+
+            }
+        }
+        else if(CurState == "MANYPOINTS")
+        {
+            if (Fixator != null)
+            {
+                Vector4 targetPosition = _solver.transform.InverseTransformPoint(Fixator.transform.position);
+                for (int i=0; i< FixPoints.Count; i++)
+                {
+                    int particleIndex = FixPoints[i];
+                    int pIdx = _rope.solverIndices[particleIndex];
+                    // Calculate effective inverse mass:
+                    float invMass = _solver.invMasses[pIdx];
+
+                    if (invMass > 0)
+                    {
+                        // Вычисление и применение силы таскающей пружины:
+                        Vector4 position = _solver.positions[pIdx];
+                        Vector4 velocity = _solver.velocities[pIdx];
+                        Vector4 force = ((targetPosition - position) * springStiffness - velocity * springDamping) / invMass;
+                        _solver.externalForces[pIdx] = force;
+
+                        // воздействовать еще на несколько шариков
+                        Vector4 dF = force / (Interval + 1);
+                        for (int j = 1; j <= Interval; j++)
+                        {
+                            force -= dF;    // ослабить силу
+                            if (particleIndex + j < _rope.particleCount)
+                            {
+                                pIdx = _rope.solverIndices[particleIndex + j];
+                                _solver.externalForces[pIdx] = force;
+                            }
+                            if (particleIndex - j > 0)
+                            {
+                                pIdx = _rope.solverIndices[particleIndex - j];
+                                _solver.externalForces[pIdx] = force;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+    public void ThrowTo(Vector3 direct)
+    {
+        Vector3 throwDirection = direct;
+        throwDirection.y += throwDirection.magnitude / 2;
+        float value = direct.magnitude*2;
+        Vector3 solvDir = _solver.transform.InverseTransformDirection(throwDirection);
+        for (int i = 0; i < _rope.particleCount; i++)
+        {
+            int solverIndex = _rope.solverIndices[i];
+            _solver.velocities[solverIndex] = solvDir * value;
+        }
+
+    }
+
+}
---
+++ Assets/Mooring/Scripts/Sailor.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+using System;
+
+public class Sailor : MonoBehaviour
+{
+    public GameObject LHand;
+    public GameObject RHand;
+    private Animator _animator;
+    private RopeController rContr;
+
+    [NonSerialized]
+    public string[] States = { "IDLE", "FIND_ROPE", "TAKE_HANK_R", "TAKE_HANK_L", "THROW_ROPE", "DRAG_ROPE_L", "DRAG_ROPE_R", "FIX_ROPE" };
+    [NonSerialized]
+    public string CurState = "IDLE";
+
+    // канат с которым взаимодействует перс
+    public ObiRope WorkRope;
+    // направление броска каната
+    public GameObject RopeTarget;
+
+    private int _workIdx;
+
+    private void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        CurState = "IDLE";
+        _animator.Play("m_idle_neutral_01");
+    }
+
+    void Update()
+    {
+
+        // Определить, нажата ли левая кнопка мыши
+        if (Input.GetKey("left ctrl") || Input.GetKey("right ctrl"))
+        {
+            if (Input.GetKeyDown("t")) // Поднять и бросить конец
+            {
+                if (CurState == "IDLE")
+                {
+                    // воспроизведение анимации
+                    CurState = "FIND_ROPE";
+                    _animator.Play("64_26_2");
+                }
+                else
+                {
+                    // остановить анимации
+                    CurState = "IDLE";
+                    _animator.Play("m_idle_neutral_01");
+                }
+            }
+        }
+    }
+
+
+
+    // после вытягивания впереде левой руки притягивает к руке ближайший шарик рабочего каната
+    public void FindRope()
+    {
+        print("FindRope");
+        if (WorkRope == null)
+        {
+            print("Не назначен WorkRope");
+            return;
+        }
+        float minMag = 100f;
+        int minIdx = -1;    // индекс в солвере
+        ObiSolver solver = WorkRope.solver;
+        for (int i = 0; i < WorkRope.particleCount; i++)
+        {
+            int solverIndex = WorkRope.solverIndices[i];
+            Vector3 posL = solver.positions[solverIndex];
+            Vector3 pos = solver.transform.TransformPoint(posL);
+            float curMag = (pos - LHand.transform.position).magnitude;
+            if (curMag < minMag)
+            {
+                minMag = curMag;
+                minIdx = i;
+            }
+        }
+        print(minIdx);
+        if (minIdx != -1)
+        {
+            _workIdx = minIdx;
+            rContr = WorkRope.GetComponent<RopeController>();
+            if(rContr.FixPoints.Count == 0)
+            {
+                rContr.FixPoints.Add(_workIdx);
+            }
+            else
+            {
+                rContr.FixPoints[0] = _workIdx; 
+            }
+            rContr.Fixator = RHand;
+            CurState = "DRAG_ROPE_R";
+        }
+        else
+        {
+            CurState = "IDLE";
+            _animator.Play("m_idle_neutral_01");
+        }
+    }
+
+
+
+    // Взять бухту каната
+    private void TakeRopeHank()
+    {
+        print("TakeRopeHank");
+
+        if (WorkRope == null)
+        {
+            print("Не назначен WorkRope");
+            return;
+        }
+
+        rContr = WorkRope.GetComponent<RopeController>();
+        int[] points = { 100, 200, 300, 400 };
+        rContr.FixPoints.Clear();
+        rContr.FixPoints.AddRange(points);
+        rContr.Fixator = RHand;
+        CurState = "TAKE_HANK_R";
+        rContr.CurState = "MANYPOINTS";
+
+    }
+
+    private void ThrowRope()
+    {
+        print("ThrowRope");
+
+        if (WorkRope == null)
+        {
+            print("Не назначен WorkRope");
+            return;
+        }
+
+        rContr = WorkRope.GetComponent<RopeController>();
+        rContr.FixPoints.Clear();
+        rContr.Fixator = null;
+        // определим направление броска в глобальных координатах
+        Vector3 startPoint;
+        if(CurState == "TAKE_HANK_R")
+        {
+            startPoint = RHand.transform.position;
+        }
+        else
+        {
+            startPoint = LHand.transform.position;
+        }
+        rContr.ThrowTo(RopeTarget.transform.position - startPoint);
+        rContr.CurState = "FREE";
+
+        CurState = "IDLE";
+    }
+
+    private void CatchRope()
+    {
+
+    }
+
+    private void DragRope()
+    {
+
+    }
+
+    private void FixRope()
+    {
+
+    }
+
+}
