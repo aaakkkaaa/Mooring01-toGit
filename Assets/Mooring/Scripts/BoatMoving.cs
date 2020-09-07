a186c3a71598242b9f7e836829b890fa541981df
fsamin <fsamin@yandex.ru>
2020-09-07 21:06:24 +0300

    Движение лодок по маршруту

---
+++ Assets/Mooring/Scripts/BoatMoving.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMoving : MonoBehaviour
+{
+    // начало движения
+    public GameObject startPoint;
+    // угловая скорость (для вращения на месте в начале)
+    [NonSerialized] public float rotV = 15.0f;
+    // максимальная линейная скорость
+    [NonSerialized] public float maxVz = 3.0f;
+    // Ускорение 
+    [NonSerialized] public float Az = 1.5f;
+    // расстояние до точки, на котором начинается поворот
+    [NonSerialized] public float LenRot = 10.0f;
+    // расстояние до последней точки, на котором начинается тормоз
+    [NonSerialized] public float LenSlow = 15.0f;
+
+    // текущая линейная скорость
+    private float _Vz=0;
+    // ускорение при замедлении
+    private float Aslow;
+
+    // текущая точка
+    private GameObject _curP;
+    // следующая точка
+    private GameObject _nextP;
+    // направление на следующую точку
+    private Vector3 _dirToNext;
+
+    // для кривой Безье - текущий путь от P0 до Q0
+    private float _p0q0;
+    // точка, с которой начинаем поворот
+    private Vector3 _P0;
+    // точка, в которой должны закончить поворот
+    private Vector3 _P2;
+    // точка - вершина поворота
+    private Vector3 _P1;
+    // расстояние от начала поворота до вершины поворота
+    private float _p0p1;
+
+
+    // тут лежат все объекты-точки
+    private GameObject _points;
+    // возвращает путь по начальной точке
+    private BoatPathManager _pathMngr;
+    // имена всех точек пути
+    private List<string> _path;
+    // индекс текущей точки
+    private int _curIdx;
+
+    private string _state = "IDLE";
+
+    // класс со служебными функциями
+    private sAssist _assist;
+
+
+    void Start()
+    {
+        _pathMngr = FindObjectsOfType<BoatPathManager>()[0];
+        //print(_pathMngr.name);
+        _points = _pathMngr.gameObject;
+        /*
+        Vector3 startPos = startPoint.transform.position;
+        startPos.y = transform.position.y;
+        transform.position = startPos;
+        */
+        transform.position = startPoint.transform.position;
+
+        _assist = FindObjectsOfType<sAssist>()[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (startPoint != null && _state == "IDLE")
+            {
+                print(gameObject.name + " -> Начинаем плыть");
+                _path = _pathMngr.CreatePath(startPoint.name);
+                _state = "START";
+                _curIdx = 0;
+                _curP = startPoint;
+                _nextP = _points.transform.Find(_path[_curIdx + 1]).gameObject;
+                print(gameObject.name + "  START  _nextP = " + _nextP.name);
+                _Vz = 0;
+            }
+        }
+        if (_state == "START")
+        {
+            // перед началом движения поворачиваем нос к следующей точке
+            Vector3 zGlobal = transform.TransformDirection(Vector3.forward);
+            _dirToNext = _nextP.transform.position - transform.position;
+            //_dirToNext.x = _dirToNext.z = 0;
+            Quaternion rot = Quaternion.FromToRotation(zGlobal, _dirToNext);
+            float ang = rot.eulerAngles.y;
+            ang = _assist.NormalizeAngle(ang);
+
+            float ang1 = Vector3.Angle(zGlobal, _dirToNext);
+
+            //print("ang = " + ang + "     ang1 = " + ang1);
+            float dAng;
+            if (Mathf.Abs(ang) > rotV * Time.deltaTime)
+            {
+                dAng = Mathf.Sign(ang) * rotV * Time.deltaTime;
+            }
+            else
+            {
+                dAng = ang;
+                _state = "ACCELERATION";
+                print(gameObject.name + " Заканчиваем начальный поворот  dAng = " + dAng);
+            }
+            Vector3 curRot = transform.eulerAngles;
+            curRot.y += dAng;
+            transform.eulerAngles = curRot;
+
+        }
+        if (_state == "ACCELERATION")
+        {
+            if (_Vz < maxVz)
+            {
+                _Vz += Az * Time.deltaTime;
+            }
+            if (_Vz > maxVz)
+            {
+                _Vz = maxVz;
+                _state = "STRAIGHT";
+                print(gameObject.name + "  Заканчиваем ускорение");
+            }
+            CorrectDirection( _nextP.transform.position );
+            MovingStep();
+        }
+        if (_state == "STRAIGHT")
+        {
+            CorrectDirection(_nextP.transform.position );
+            MovingStep();
+
+            // расстояние до ближайшей точки
+            float len = (transform.position - _nextP.transform.position).magnitude;
+            if (_curIdx == _path.Count - 2)
+            {
+                // предпоследняя точка, проверяем, не пора ли замедляться
+                if (len < LenSlow)
+                {
+                    _state = "SLOW";
+                    Aslow = _Vz * _Vz / 2 / len;
+                    print(gameObject.name + "  Останавливаемся   Aslow = " + Aslow);
+                }
+            }
+            else
+            {
+                // проверяем расстояние до след. точки чтобы начать разворот
+                if (len < LenRot)
+                {
+                    _state = "ROTATION";
+                    _P0 = transform.position;
+                    // точка окончания поворота лежит на прямой, соединяющей следующие две маршрутные точки
+                    GameObject next2P = _points.transform.Find(_path[_curIdx + 2]).gameObject;
+                    Vector3 nextDir = next2P.transform.position - _nextP.transform.position;
+                    nextDir.y = transform.position.y;
+                    nextDir = nextDir.normalized;
+                    // отступаем от следующей точки в найденном направлении на расстояние len
+                    _P2 = _nextP.transform.position + nextDir * len;
+                    _P2.y = transform.position.y;
+                    _P1 = _nextP.transform.position;
+                    _P1.y = transform.position.y;
+                    _p0q0 = 0;
+                    _p0p1 = (_P1 - _P0).magnitude;
+                    print(gameObject.name + " ROTATION   _P0 = " + _P0 + "   _P1 = " + _P1 + "    _P2 = " + _P2);
+
+                }
+            }
+            return;
+        }
+        if (_state == "SLOW")
+        {
+            //CorrectDirection();
+            _Vz -= Aslow*Time.deltaTime;
+            if (_Vz <= 0)
+            {
+                _Vz = 0;
+                _state = "IDLE";
+                print(gameObject.name + " Остановились");
+                // установить новую текущую точку
+                startPoint = _points.transform.Find(_path[_path.Count - 1]).gameObject;
+
+            }
+            else
+            {
+                MovingStep();
+            }
+        }
+        if (_state == "ROTATION")
+        {
+            float dVz = _Vz * Time.deltaTime;
+            float newL = _p0q0 + dVz;
+            // не пора ли выходить из разворота
+            if (newL >= _p0p1)
+            {
+                _state = "STRAIGHT";
+                _curP = _nextP;
+                _curIdx++;
+                _nextP = _points.transform.Find(_path[_curIdx + 1]).gameObject;
+            }
+            else
+            {
+                // первая итерация, предварительная
+                float testT = newL / _p0p1;
+                Vector3 Q0test = _P0 + (_P1 - _P0) * testT;
+                Vector3 Q1test = _P1 + (_P2 - _P1) * testT;
+                Vector3 Btest = Q0test + (Q1test - Q0test) * testT;
+                // полученное расстояние отличается от того, которое лодка проходит, вычисляем на сколько надо скорректировать
+                float correction = (Btest - transform.position).magnitude / dVz;
+                //print("correction = " + correction);
+                // вторая итерация, находим точку с учетом коррекции
+                float realL = _p0q0 + dVz / correction;
+                float realT = realL / _p0p1;
+                Vector3 Q0 = _P0 + (_P1 - _P0) * realT;
+                Vector3 Q1 = _P1 + (_P2 - _P1) * realT;
+                Vector3 Breal = Q0 + (Q1 - Q0) * realT;
+                // перемещение и разворот
+                transform.position = Breal;
+                CorrectDirection(Q1test);
+                _p0q0 += _Vz * Time.deltaTime / correction;
+            }
+        }
+    }
+
+    // смещение на каждом шаге
+    private void MovingStep()
+    {
+        Vector3 locPos = new Vector3(0, 0, _Vz*Time.deltaTime);
+        Vector3 pos = transform.TransformPoint(locPos);
+        transform.position = pos;
+    }
+
+    // коррекция направления при  движении к следующей точке 
+    private void CorrectDirection( Vector3 nextPosition )
+    {
+        Vector3 zGlobal = transform.TransformDirection(Vector3.forward);
+        _dirToNext = nextPosition - transform.position;
+        Quaternion rot = Quaternion.FromToRotation(zGlobal, _dirToNext);
+        float ang = rot.eulerAngles.y;
+        ang = _assist.NormalizeAngle(ang);
+        Vector3 curRot = transform.eulerAngles;
+        curRot.y += ang;
+        transform.eulerAngles = curRot;
+    }
+
+}
---
+++ Assets/Mooring/Scripts/BoatPathManager.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+//using System;
+
+public class BoatPathManager : MonoBehaviour
+{
+    // пути
+    private string[][] _branches;
+
+    // номер текущего пути
+    private int _curBranch;
+    // последняя пройденая точка пути
+    private int _curPoint;
+    // направление движения по массиву точект
+    private int _pathDir;
+    // путь из имен точек
+    List<string> _result;
+
+
+    void Start()
+    {
+        _branches = new string[10][];
+        _branches[0] = new string[] { "P02", "P01", "P03" };
+        _branches[1] = new string[] { "P03", "P18" };
+        _branches[2] = new string[] { "P03", "P04" };
+        _branches[3] = new string[] { "P04", "P05" };
+        _branches[4] = new string[] { "P04", "P06" };
+        _branches[5] = new string[] { "P06", "P07", "P08", "P09" };
+        _branches[6] = new string[] { "P06", "P10" };
+        _branches[7] = new string[] { "P10", "P11" };
+        _branches[8] = new string[] { "P10", "P12", "P13" };
+        _branches[9] = new string[] { "P10", "P14", "P15", "P16", "P17" };
+
+        //CreatePath("P10");
+    }
+
+    public List<string> CreatePath(string cur)
+    {
+        // так можно задать произвольный конкретный путь
+        //return new List<string>(new string[] { "P02", "P02_01", "P02_02", "P02_03" });
+
+        _result = new List<string>();
+        _result.Add(cur);
+        _curBranch = -1;
+        while ( CreateBranch(cur, _curBranch) )
+        {
+            cur = _result[_result.Count - 1];
+        }
+
+        PrintResult(_result);
+        return _result;
+    }
+
+    // по стартовой точке строит часть пути до пересечения или до конца (в этом случае возвращает false)
+    private bool CreateBranch(string cur, int exclude)
+    {
+        // определим скольким возможным путям соответствует точка cur
+        List<int> start = new List<int>();
+        for (int i = 0; i < _branches.Length; i++)
+        {
+            for (int j = 0; j < _branches[i].Length; j++)
+            {
+                if (cur == _branches[i][j] && exclude != i)
+                {
+                    start.Add(i);
+                }
+            }
+        }
+        print("CreatePath -> " + start.Count);
+
+        // выбираем ветку из числа содержащих начальный узел cur
+        _curBranch = -1;
+        if (start.Count == 0)
+        {
+            print("Ошибка построения пути, start.Count == 0, заканчиваем поиск пути");
+            return false;
+        }
+        if (start.Count == 1)
+        {
+            _curBranch = start[0];
+        }
+        else
+        {
+            int num = Mathf.FloorToInt(Random.Range(0, start.Count));
+            _curBranch = start[num];
+        }
+        if (_curBranch == -1)
+        {
+            print("Ошибка построения пути, _curBranch == -1, заканчиваем поиск пути");
+            return false;
+        }
+        string[] branch = _branches[_curBranch];        // будем двигаться по этой ветке
+
+        // направление движения по ветке - от начала +1, от конца -1
+        int idx = System.Array.IndexOf(branch, cur);    // где на этой ветке стартовая точка
+        if (idx == 0)
+        {
+            _pathDir = 1;
+        }
+        else if (idx == branch.Length - 1)
+        {
+            _pathDir = -1;
+        }
+        else
+        {
+            print("Ошибка построения пути, начальная точка на середине ветки. idx = " + idx);
+        }
+        // стартовая точка, ветка и направление определено, надо сгенерировать путь до следующего пересечения
+        do
+        {
+            idx += _pathDir;
+            _result.Add(branch[idx]);
+            for (int i = 0; i < _branches.Length; i++)
+            {
+                if (i != _curBranch)
+                {
+                    for (int j = 0; j < _branches[i].Length; j++)
+                    {
+                        if ( branch[idx] == _branches[i][j] )
+                        {
+                            return true;    // найдена точка ветвления
+                        }
+                    }
+                }
+            }
+
+        } while ( !(idx == 0 || idx == branch.Length-1) );
+
+
+        return false;   // дошли до конца ветки
+    }
+
+    private void PrintResult(List<string> res)
+    {
+        string str = "";
+        for (int i = 0; i < res.Count; i++)
+        {
+            if (i == res.Count - 1)
+            {
+                str += res[i];
+            }
+            else
+            {
+                str += (res[i] + " - ");
+            }
+        }
+        print(str);
+    }
+
+
+}
